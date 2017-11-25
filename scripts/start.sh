9257485fdbba133f8de67cb504f30e13ea555bf8
tisu0813 <timur.sultanaev@netcracker.com>
2017-11-25 22:29:53 +0300

    init commit

---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ln -s /data /var/opengrok
+
+wait_for_opengrok_and_reindex(){
+    # wait for tomcat startup
+    sleep 10
+    while ! ( ps aux|grep -q org.apache.catalina.startup.Bootstrap ); do
+        sleep 1;
+    done
+    OpenGrok index /src
+}
+
+wait_for_opengrok_and_reindex &
+
+catalina.sh run
